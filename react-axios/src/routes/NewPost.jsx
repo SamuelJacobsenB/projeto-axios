3641d6e99d4acb552e33f315e33d3e2104c73dbc
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-12 12:26:04 -0300

    Projeto terminado

--- react-axios/src/routes/NewPost.jsx
+++ react-axios/src/routes/NewPost.jsx
@@ -1,17 +1,43 @@
+import { useState } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
+import blogFetch from '../axios/config';
+
 import './NewPost.css';
 
 const NewPost = () => {
+
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState();
+  const [body, setBody] = useState();
+
+  const createPost = async(e)=>{
+    e.preventDefault();
+
+    const post = {title, body, userId: 1};
+
+    await blogFetch.post('/posts', {
+      body: post
+    }).then(()=>{
+      navigate('/');
+    }).catch((error)=>{
+      console.log(error);
+    });
+  };
+
   return (
     <div className='new-post'>
       <h2>Inserir novo Post:</h2>
-      <form action="" method="post" className='form-control'>
+      <form onSubmit={(e)=>createPost(e)}>
         <div className="form-control">
           <label htmlFor="title">Título:</label>
-            <input type="text" name="title" id="title" placeholder='Digite o título' required/>
+            <input type="text" name="title" id="title" placeholder='Digite o título' required onChange={(e)=>setTitle(e.target.value)}/>
         </div>
         <div className="form-control">
           <label htmlFor="body">Conteúdo:</label>
-            <textarea name="body" id="body" placeholder='Digite o conteúdo' required></textarea>
+            <textarea name="body" id="body" placeholder='Digite o conteúdo' required onChange={(e)=>setTitle(e.target.value)}></textarea>
         </div>
         <input type="submit" value="Criar Post" className='btn'/>
       </form>
