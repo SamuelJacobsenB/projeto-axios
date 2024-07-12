d56a8924d07ddc748bc24a994b0a3a44d1306e03
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-12 11:03:07 -0300

    Começando com axios

--- react-axios/src/routes/Home.jsx
+++ react-axios/src/routes/Home.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+
+import axios from 'axios';
+
+import { Link } from 'react-router-dom';
+
+import './Home.css';
 
 const Home = () => {
+
+  const [post, setPost] = useState([]);
+
+  const getPosts = async()=>{
+    try {
+      const response = axios.get('https://jsonplaceholder.typicode.com/posts');
+      
+      const data = (await response).data;
+      console.log(data);
+    } catch(error){
+        console.log(error);
+    };
+  };
+
+  useEffect(()=>{
+    getPosts();
+  }, []);
+
   return (
     <div>Home</div>
   );
